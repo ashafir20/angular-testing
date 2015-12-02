71aca1f02d12bf4ecb55e86475659b01cc32a054
Aviv Shafir <ashafir20@gmail.com>
2015-12-02 20:52:27 +0200

    more samples for resource

--- spec/movie-core/service.spec.js
+++ spec/movie-core/service.spec.js
@@ -11,6 +11,7 @@ describe('MovieCore', function() {
 	
 	afterEach(function(){
 		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
 	})
 	
 	it('should create popular movie', function() {
@@ -53,6 +54,36 @@ describe('MovieCore', function() {
 		});
 		
 		popularMovie.$update();
+		
+		expect($httpBackend.flush).not.toThrow();
+	});
+	
+	it('should authenticate requests', function() {
+		var headerData = function(headers) {
+			return headers.authToken === 'teddybear';
+		}
+		
+    	var matchAny = /.*/;
+
+		$httpBackend.whenGET(matchAny, headerData)
+			.respond(200);
+
+		$httpBackend.expectPOST(matchAny, matchAny, headerData)
+			.respond(200);
+
+		$httpBackend.expectPUT(matchAny, matchAny, headerData)
+			.respond(200);
+
+		$httpBackend.expectDELETE(matchAny, headerData)
+			.respond(200);
+
+		var popularMovie = { id: 'tt0076759', description: 'This movie is great!' };
+
+		PopularMovies.query();
+		PopularMovies.get({ id: 'tt0076759' });
+		new PopularMovies(popularMovie).$save();
+		new PopularMovies(popularMovie).$update();
+		new PopularMovies(popularMovie).$remove();
 		
 		expect($httpBackend.flush).not.toThrow();
 	});
